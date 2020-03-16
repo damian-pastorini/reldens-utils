301ac9e2a3d47c69bfef20b88357d32073286d5f
Damian A. Pastorini <damian.pastorini>
2020-03-17 00:00:56 +0100

    - Reldens - Utils

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * Reldens - Utils
+ *
+ */
+
+module.exports = {
+    EventsManager: require('./lib/events-manager'),
+    ErrorManager: require('./lib/error-manager'),
+    Logger: require('./lib/logger')
+};
