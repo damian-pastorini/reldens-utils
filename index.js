4b132952507ee54f82eb69b4c2565f8421984877
Damian A. Pastorini <damian.pastorini>
2020-09-26 21:56:08 +0200

    - Reldens - Utils

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ module.exports = {
     EventsManager: require('./lib/events-manager'),
     ErrorManager: require('./lib/error-manager'),
     InteractionArea: require('./lib/interaction-area'),
-    Logger: require('./lib/logger')
+    Logger: require('./lib/logger'),
+    sc: require('./lib/short-cuts')
 };
