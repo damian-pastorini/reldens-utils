e2706978485f327a6bc8982ca75d54711a2fef8f
Damian A. Pastorini <damian.pastorini@gmail.com>
2025-07-27 22:40:44 +0200

    - Reldens - Utils

--- tests/run.js
+++ tests/run.js
@@ -1,18 +1,10 @@
 console.log('Running all tests...\n');
 
 try{
-    /*
-    console.log('='.repeat(60));
-    console.log('TESTING OLD IMPLEMENTATION');
-    console.log('='.repeat(60));
-    require('./test-old.js');
-
-    console.log('\n\n');
-    */
     console.log('='.repeat(60));
     console.log('TESTING NEW IMPLEMENTATION');
     console.log('='.repeat(60));
-    require('./test-new.js');
+    require('./events-manager-test.js');
 
     console.log('\n\nAll tests completed successfully!');
 } catch(error){
